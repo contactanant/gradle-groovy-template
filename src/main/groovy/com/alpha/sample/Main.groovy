aacc59b5e54516bb122077371de5ee5f64ebe8f6
Anant Agarwal <contactanant@yahoo.com>
2015-04-28 22:34:02 +0100

    Anant | Added gradle template project

---
+++ src/main/groovy/com/alpha/sample/Main.groovy
@@ -0,0 +1,11 @@
+package com.alpha.sample
+
+/**
+ * Created by anantagarwal on 30/03/2015.
+ */
+class Main {
+    public static void main(String[] args) {
+        def currentTime = new Date()
+        println "Current Time is $currentTime"
+    }
+}
